6b6546539b45215472e75f880def93d598172e30
David Shepherd <davidshepherd7@gmail.com>
2017-06-23 08:36:20 +0100

    Make dependencies consistent

--- dropbox-conflicts.el
+++ dropbox-conflicts.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/jezcope/dropbox-conflicts
 ;; Keywords: convenience
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.4") (f "0.19.0"))
 
 ;; This file is NOT part of GNU Emacs.
 
